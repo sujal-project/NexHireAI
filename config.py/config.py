6d8f3d5cbb29403ee813623ff3d6ccf40b0bd746
Sujal Gaikwad <sujalgaikwad2025@outlook.com>
2026-04-22 18:56:14 +0530

    add config.py code

---
+++ config.py/config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    SECRET_KEY = "supersecretkey"   # change in production
+
+    # Upload config
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB max file
+
+    # Allowed file types
+    ALLOWED_EXTENSIONS = {"pdf"}
+
+    # Database (ODBC)
+    DB_CONNECTION_STRING = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=localhost;"
+        "DATABASE=NexHireAI;"
+        "Trusted_Connection=yes;"
+    )
